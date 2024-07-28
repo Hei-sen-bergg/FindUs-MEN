212557c6a05737ad3b339872ebdf3d1bb3f2a53f
Hei-sen-bergg <senrajshekar98@gmail.com>
2024-07-28 16:46:49 +0530

    vercel.json file added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import '../src/scss/index.scss'
-import { AuthContextProvider } from './context/AuthContext.jsx'
-import { SocketContextProvider } from './context/SocketContext.jsx'
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthContextProvider>
-    <SocketContextProvider>
-    <App />
-    </SocketContextProvider>   
-    </AuthContextProvider>
-  </React.StrictMode>,
-)
+  
