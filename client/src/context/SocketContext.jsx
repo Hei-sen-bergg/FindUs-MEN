ee5c64d3daf402ab40d423cb64ac4071eb5bca29
Hei-sen-bergg <senrajshekar98@gmail.com>
2024-07-28 14:35:32 +0530

    updated baseurl

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -11,7 +11,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("http://localhost:4000"));
+    setSocket(io("https://findus-mern.onrender.com"));
   }, []);
 
   useEffect(() => {
