7504a918db8ed7481f83d010da45016225bf9920
ZN <154010547+Hei-sen-bergg@users.noreply.github.com>
2024-07-29 23:43:56 +0530

    Update authController.js
    sameSite corrected

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -62,10 +62,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: true,                   //enable in production
-<<<<<<< HEAD
         sameSite: "none",
-=======
->>>>>>> 85bcf1ce12cd333894ebf47ed671f7b78a77a222
         maxAge: age,
       })
       .status(200)
